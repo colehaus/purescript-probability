23c4ffbccb749a4a770728807826f946b89495d1
Cole Haus <colehaus@cryptolab.net>
2019-02-11 15:21:14 -0800

    Remove `insertWith`
    It's been added to `Data.Map`

--- src/Data/Map/Extras.purs
+++ src/Data/Map/Extras.purs
@@ -34,6 +34,3 @@ mapKeysMaybeWithValueWith :: forall j k v. Ord j => Ord k => (k -> Maybe j) -> (
 mapKeysMaybeWithValueWith f g m = foldlWithIndex h mempty m
   where
     h k acc v = maybe acc (flip (uncurry insert) acc) $ g k v <<< (flip lookup acc) =<< f k
-
-insertWith :: forall k v. Ord k => (v -> v -> v) -> k -> v -> Map k v -> Map k v
-insertWith f k v = Map.unionWith f (Map.singleton k v)
--- src/Math/Probability.purs
+++ src/Math/Probability.purs
@@ -11,7 +11,6 @@ import Data.List (List(Nil), (:))
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Map.Extras (mapKeysMaybe, mapKeysWith)
-import Data.Map.Extras as Map
 import Data.Maybe (Maybe)
 import Data.NonEmpty (NonEmpty)
 import Data.NonEmpty.Extras as NonEmpty
